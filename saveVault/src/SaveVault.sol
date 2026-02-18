cf7cd4266fd321c38c43d912a9e8400a168e6db8
Justice <onyekaozuru122@gmail.com>
2026-02-18 20:48:48 +0100

    feat: withdraw deposit and added interface

--- saveVault/src/SaveVault.sol
+++ saveVault/src/SaveVault.sol
@@ -1,39 +1,56 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+interface IERC20 {
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+    function transfer(address to, uint256 amount) external returns (bool);
+}
+
+
 contract saveVault {
 
-    // mapping(address => uint256) public ethBalances;
-    mapping(address => uint256) tokenBalances;
     mapping(address => uint256) public ethBalances;
+    mapping(address => mapping(address => uint256)) public tokenBalances;  
 
-    function deposithETh()external payable {
+    event ETHDeposited(address indexed user, uint256 amount);
+    event ETHWithdrawn(address indexed user, uint256 amount);
+    event TokenDeposited(address indexed user, address indexed token, uint256 amount);
+    event TokenWithdrawn(address indexed user, address indexed token, uint256 amount);  
+
+    function deposithETH()external payable {
         require(msg.value > 0, "Deposit must be greater than 0");
         ethBalances[msg.sender] += msg.value;
+        emit ETHDeposited(msg.sender, msg.value);
     }
 
-    function withdrawEth(uint256 amount) external {
+    function withdrawETH(uint256 amount) external {
         require(ethBalances[msg.sender] >= amount, "Insufficient balance");
         ethBalances[msg.sender] -= amount;
-        payable(msg.sender).transfer(amount);
+        (bool sent, ) = msg.sender.call{value: amount}("");
+        require(sent, "ETH transfer failed");
+         emit ETHWithdrawn(msg.sender, amount);
     }
 
-    function depositToken(uint256 amount) external {
-        require(amount > 0, "Deposit must be greater than 0");
-        tokenBalances[msg.sender] += amount;
+   function depositToken(address token, uint256 amount) external {
+        require(amount > 0, "Amount must be > 0");
+        IERC20(token).transferFrom(msg.sender, address(this), amount);
+        tokenBalances[msg.sender][token] += amount;
+        emit TokenDeposited(msg.sender, token, amount);
     }
 
-    function withdrawToken(uint256 amount) external {
-        require(tokenBalances[msg.sender] >= amount, "Insufficient balance");
-        tokenBalances[msg.sender] -= amount;
+    function withdrawToken(address token, uint256 amount) external {
+        require(tokenBalances[msg.sender][token] >= amount, "Insufficient balance");
+        tokenBalances[msg.sender][token] -= amount;
+        IERC20(token).transfer(msg.sender, amount);
+        emit TokenWithdrawn(msg.sender, token, amount);
     }
 
     function getEthBalance() external view returns (uint256) {
         return ethBalances[msg.sender];
     }
 
-    function getTokenBalance() external view returns (uint256) {
-        return tokenBalances[msg.sender];
+    function getTokenBalance(address token) external view returns (uint256) {
+        return tokenBalances[msg.sender][token];
     }   
 
 }
